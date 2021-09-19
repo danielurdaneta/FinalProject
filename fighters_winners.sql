8afdfb1eeb7a1e15268ed596e24a08b049cacc36
Julia Cho <heejaecho.1994@gmail.com>
2021-09-19 15:40:09 -0500

    adding schema for joining two tables

---
+++ fighters_winners.sql
@@ -0,0 +1,8 @@
+-- Creating winners table for Final Project
+CREATE TABLE fighters_winners (
+	"fightID" FLOAT,
+	"R_fighter" VARCHAR(40),
+	"B_fighter" VARCHAR(40),
+	"date" DATE,
+	"Winner" FLOAT
+);
